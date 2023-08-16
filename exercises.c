ae09792117651a77c453429200a9e84d28107027
Replit user <>
2023-08-16 01:52:08 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -112,14 +112,14 @@ int checkSorted(int arr[], int size) {
   size_t i = 0;
   size_t j = 0;
   
-  while(i < size - 1 && arr[i] <= arr[i + 1]) {
+  while(i < size && arr[i] <= arr[i + 1]) {
     if(i == size - 1) {
       return 1;
     }
     i++;
   }
   
-  while(j < size - 1 && arr[j] >= arr[j + 1]) {
+  while(j < size && arr[j] >= arr[j + 1]) {
     if(j == size - 1) {
       return -1;
     }
